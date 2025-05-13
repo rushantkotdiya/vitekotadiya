c528aaf5a4b05b57d5a4aeac51eae3e54d5f7783
kotadiya rushant <kotadiyarushant@gmail.com>
2025-05-13 10:32:36 +0530

    add: initial files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css'
+import '../src/componets/styles/style.css'
 import Home from './home';
 import Pn from './Pn';
 import Index from './Index';
